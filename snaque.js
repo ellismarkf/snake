41f25162ba06b4ca5b6acbfb0b1ea5cf607557a8
Mark Ellis <ellismarkf@gmail.com>
2017-06-27 16:43:59 -0500

    ✨ - track individaul segment direction

--- snaque.js
+++ snaque.js
@@ -4,6 +4,7 @@ const MAX_CAPACITY = (1 << 30) | 0;
 const MIN_CAPACITY = 16;
 const X = 0;
 const Y = 1;
+const DIR = 2;
 
 function nearestPowerOf2(n) {
     n = n >>> 0;
@@ -38,18 +39,11 @@ function Snaque(capacity) {
   this._tail = 0;
   this._x = new Int32Array(this._capacity).fill(-1);
   this._y = new Int32Array(this._capacity).fill(-1);
+  this._directions = new Int32Array(this._capacity).fill(-1);
 }
 
 Snaque.prototype._nextTailIdx = function Snaque$_nextTailIdx() {
   return (this._head + this._length) & (this._capacity - 1);
-}
-
-Snaque.prototype._prevTailIdx = function Snaque$_prevTailIdx() {
-  // let nextLen = this._length - 2;
-  // return nextLen < -1
-  //   ? 0
-  //   : (this._head + nextLen) & (this._capacity - 1);
-  return (this._head + this._length - 2) & (this._capacity - 1);
 }
 
 Snaque.prototype._getTailIdx = function Snaque$_getTailIdx() {
@@ -69,13 +63,14 @@ Snaque.prototype._getIdx = function Snaque$_getIdx(n) {
   return (this._head + n) & (this._capacity - 1);
 }
 
-Snaque.prototype.push = function Snaque$push(coords) {
+Snaque.prototype.push = function Snaque$push(data) {
   let i = this._nextTailIdx();
   if (i > this._capacity || i < 0) {
     throw new RangeError('New index out of bounds. ', i);
   }
-  this._x[i] = coords[X];
-  this._y[i] = coords[Y];
+  this._x[i] = data[X];
+  this._y[i] = data[Y];
+  this._directions[i] = data[DIR];
   this._length += 1;
   this._tail = i;
   return this._length;
@@ -83,12 +78,13 @@ Snaque.prototype.push = function Snaque$push(coords) {
 
 Snaque.prototype.pop = function Snaque$pop() {
   if (this._length === 0) {
-    return [-1, -1];
+    return [-1, -1, -1];
   }
   let i = this._getTailIdx();
-  let ret = [this._x[i], this._y[i]];
+  let ret = [this._x[i], this._y[i], this._directions[i]];
   this._x[i] = -1;
   this._y[i] = -1;
+  this._directions[i] = -1;
   this._length -= 1;
   this._tail = this._getTailIdx();
   return ret;
@@ -96,25 +92,27 @@ Snaque.prototype.pop = function Snaque$pop() {
 
 Snaque.prototype.shift = function Snaque$shift() {
   if (this._length === 0) {
-    return [-1, -1];
+    return [-1, -1, -1];
   }
   let head = this._head;
-  let ret = [this._x[head], this._y[head]];
+  let ret = [this._x[head], this._y[head], this._directions[head]];
   this._x[head] = -1;
   this._y[head] = -1;
+  this._directions[head] = -1;
   this._head = this._nextHeadIdx();
   this._length -= 1;
   return ret;
 }
 
-Snaque.prototype.unshift = function Snaque$unshift(coords) {
+Snaque.prototype.unshift = function Snaque$unshift(data) {
   let capacity = this._capacity;
   let i = this._prevHeadIdx();
   if (i > capacity || i < 0) {
     throw new RangeError('New index out of bounds. ', i);
   }
-  this._x[i] = coords[X];
-  this._y[i] = coords[Y];
+  this._x[i] = data[X];
+  this._y[i] = data[Y];
+  this._directions[i] = data[DIR];
   this._length += 1;
   this._head = i;
   return length + 1;
@@ -124,7 +122,7 @@ Snaque.prototype.unshift = function Snaque$unshift(coords) {
 Snaque.prototype.forEach = function Snaque$forEach(fn) {
   for (let n = 0; n < this._length; n++) {
     let i = this._getIdx(n);
-    fn.call(this, [this._x[i], this._y[i]], n, i, this);
+    fn.call(this, [this._x[i], this._y[i], this._directions[i]], n, i, this);
   }
 }
 
@@ -132,7 +130,7 @@ Snaque.prototype.every = function Snaque$every(fn) {
   let pass = true;
   for (let n = 0; n < this._length; n++) {
     let i = this._getIdx(n);
-    let val = fn.call(this, [this._x[i], this._y[i]], n, i, this);
+    let val = fn.call(this, [this._x[i], this._y[i], this._directions[i]], n, i, this);
     if (!val) {
       pass = false;
       break;
@@ -145,7 +143,7 @@ Snaque.prototype.everyFrom = function Snaque$everyFrom(fn, start) {
   let pass = true;
   for (let n = start || 0; n < this._length; n++) {
     let i = this._getIdx(n);
-    let val = fn.call(this, [this._x[i], this._y[i]], n, i, this);
+    let val = fn.call(this, [this._x[i], this._y[i], this._directions[i]], n, i, this);
     if (!val) {
       pass = false;
       break;
@@ -158,7 +156,7 @@ Snaque.prototype.findIndex = function Snaque$findIndex(fn) {
   let index = -1;
   for (let n = 0; n < this._length; n++) {
     let i = this._getIdx(n);
-    let val = fn.call(this, [this._x[i], this._y[i]], n, i, this);
+    let val = fn.call(this, [this._x[i], this._y[i], this._directions[i]], n, i, this);
     if (val) {
       index = i;
       break;
@@ -173,9 +171,10 @@ Snaque.prototype.sliceFrom = function Snaque$sliceFrom(begin) {
   for (let n = 0; n < length; n++) {
     let i = this._getIdx(n);
     if (i >= begin) {
-      sliced.push([this._x[i], this._y[i]]);
+      sliced.push([this._x[i], this._y[i], this._directions[i]]);
       this._x[i] = -1;
       this._y[i] = -1;
+      this._directions[i] = -1;
     }
   }
   this._length = length - sliced.length;
@@ -184,34 +183,33 @@ Snaque.prototype.sliceFrom = function Snaque$sliceFrom(begin) {
 
 Snaque.prototype.peekTail = function Snaque$peekTail() {
   if (this._length === 0) {
-    return [-1, -1];
+    return [-1, -1, -1];
   }
-  return [this._x[this._tail], this._y[this._tail]];
+  return [this._x[this._tail], this._y[this._tail], this._directions[this._tail]];
 };
 
 Snaque.prototype.peekHead = function Snaque$peekHead() {
   if (this._length === 0) {
-    console.log('peekHead returning invalid')
-    return [-1, -1];
+    return [-1, -1, -1];
   }
-  return [this._x[this._head], this._y[this._head]];
+  return [this._x[this._head], this._y[this._head], this._directions[this._head]];
 };
 
 
 Snaque.prototype.get = function Snaque$get(index) {
   let n = index;
   if ((n !== (n | 0))) {
-    return [-1, -1];
+    return [-1, -1, -1];
   }
   let len = this._length;
   if (n < 0) {
     n += len;
   }
   if (n < 0 || n >= len) {
-    return [-1, -1];
+    return [-1, -1, -1];
   }
   let i = this._getIdx(n);
-  return [this._x[i], this._y[i]];
+  return [this._x[i], this._y[i], this._directions[i]];
 };
 
 Snaque.prototype.isEmpty = function Snaque$isEmpty() {
@@ -226,6 +224,7 @@ Snaque.prototype.clear = function Snaque$clear() {
     let i = this._getIdx(j);
     this._x[i] = -1;
     this._y[i] = -1;
+    this._directions[i] = -1;
   }
   this._length = 0;
   this._head = 0;
@@ -239,9 +238,8 @@ Snaque.prototype.toArray = function Snaque$toArray() {
   let capacity = this._capacity;
   for (let j = 0; j < len; ++j) {
     let i = this._getIdx(j);
-    ret[j] = [this.x[i], this.y[i]];
+    ret[j] = [this.x[i], this.y[i], this._directions[i]];
   }
-  console.log('toArray');
   return ret;
 };
 
@@ -251,6 +249,7 @@ Snaque.prototype.toString = function Snaque$toString() {
 
 Snaque.prototype.toJSON = Snaque.prototype.toArray;
 Snaque.prototype.head = Snaque.prototype.peekHead;
+Snaque.prototype.tail = Snaque.prototype.peekTail;
 
 Object.defineProperties(Snaque.prototype, {
   length: {
